a9c328ec73a339e31e1f97cd1d9be733c40ae0f0
Adolfo <aapc972@gmail.com>
2022-07-27 19:55:11 -0400

    exercise 2.1 courseinfo

--- courseinfo/src/components/Content.js
+++ courseinfo/src/components/Content.js
@@ -1,5 +1,5 @@
 import Part from "./Part"
 
-const Content = ({course}) => course.parts.map((part, i) => <Part key={i} part={part}/>)
+const Content = ({course}) => course.parts.map(part => <Part key={part.id} part={part}/>)
 
 export default Content
