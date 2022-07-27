82bd8f64b465b4ff209ad5b3fc715a12fc77668b
Adolfo <aapc972@gmail.com>
2022-07-27 19:44:44 -0400

    exercise 2.3 courseinfo

--- courseinfo/src/components/Total.js
+++ courseinfo/src/components/Total.js
@@ -1,6 +1,3 @@
-const Total = ({course}) => {
-  const exercises = course.parts.map(part => part.exercises)
-  return <h3>total of {exercises.reduce((s, p) => s + p)} exercises</h3>
-}
+const Total = ({course}) => <h3>total of {course.parts.map(part => part.exercises).reduce((s, p) => s + p)} exercises</h3>
 
 export default Total
