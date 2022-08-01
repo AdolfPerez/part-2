1c84fd4b81bc0813609e4ef169b133ef81b6567e
Adolfo <aapc972@gmail.com>
2022-07-31 20:11:03 -0400

    exercise 2.11 phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '9876543210' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  const [ toPrint, setToPrint ] = useState(persons)
+  const [ persons, setPersons ] = useState([]) 
+  const [ toPrint, setToPrint ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(({data}) => {
+      setPersons(data)
+      setToPrint(data)
+    })
+  }, [])
 
   const onSubmit = event => {
     event.preventDefault()
